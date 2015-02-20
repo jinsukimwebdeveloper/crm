d037a007ee6ad45d1d9a2e12b7d1d78e7ada3181
jinsukimwebdeveloper <jinsukimwebdeveloper@outlook.kr>
2015-02-20 21:04:48 +0900

    The first commit

---
+++ crm/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using crm.ViewModel.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model)
+        {
+            if (ModelState.IsValid) { }
+
+            ViewData["LoginIdDoesnotExist"] = @Resources.Resources.LoginIdDoesnotExist;
+            return View("Index");
+        }
+    }
+}
---
+++ crm/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace crm
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void Application_BeginRequest(Object source, EventArgs e)
+        {
+            string culture;
+            if (Request.Cookies["lang"] != null)
+            {
+                culture = Request.Cookies["lang"].Value;
+                culture = string.IsNullOrEmpty(culture) ? "en-US" : culture;
+            }
+            else
+            {
+                // Browser's default language setting value
+                culture = Request.UserLanguages[0];
+            }
+
+            HttpApplication application = (HttpApplication)source;
+            HttpContext context = application.Context;
+
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+        }
+    }
+}
